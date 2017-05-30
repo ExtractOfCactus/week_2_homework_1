36f18cea3196ceb3df293047966fd6c2cb9af504
Glen Young <extractofcactus@icloud.com>
2017-05-30 07:13:46 +0100

    C parts 1&2 passed

--- library/library.rb
+++ library/library.rb
@@ -7,8 +7,14 @@ class Library
   end
 
   def all_books()
-    
+    return @books
+  end
 
+  def find_book_by_title(title)
+    for book in @books
+      return book if book[:title] == title
+    end
+    return nil
   end
 
 end
